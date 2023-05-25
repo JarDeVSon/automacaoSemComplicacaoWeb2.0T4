987c38de1f885182afccd1dcee133b9d039ebd11
Jardeson Santos <jardounifanor@gmail.com>
2023-05-25 20:07:11 -0300

    Initial commit

---
+++ src/test/java/br/com/chronosacademy/pages/LoginPage.java
@@ -0,0 +1,86 @@
+package br.com.chronosacademy.pages;
+
+import br.com.chronosacademy.core.Driver;
+import br.com.chronosacademy.maps.LoginMap;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    LoginMap loginMap;
+
+    public LoginPage() {
+        loginMap = new LoginMap();
+        PageFactory.initElements(Driver.getDriver(), loginMap);
+    }
+
+    public void clickBtnLogin(){
+        Driver.visibilityOf(loginMap.btnLogin);
+        loginMap.btnLogin.click();
+    }
+
+    public void clickBtnFechar(){
+        Driver.visibilityOf(loginMap.btnFechar);
+        loginMap.btnFechar.click();
+    }
+
+    public void clickDivFechaModal(){
+        Driver.visibilityOf(loginMap.divFechaModal);
+        loginMap.divFechaModal.click();
+    }
+
+    public void setInpUserName(String username){
+        Driver.visibilityOf(loginMap.inpUserName);
+        if (username != null){
+            loginMap.inpUserName.sendKeys(username);
+        }
+    }
+
+    public void setInpPassword(String password){
+        Driver.visibilityOf(loginMap.inpPassword);
+        if (password != null){
+            loginMap.inpPassword.sendKeys(password);
+        }
+    }
+
+    public void clickInpRemember(){
+        Driver.visibilityOf(loginMap.inpRemember);
+        loginMap.inpRemember.click();
+    }
+
+    public void clickLinkCreateAccount(){
+        Driver.visibilityOf(loginMap.linkCreateAccount);
+        loginMap.linkCreateAccount.click();
+    }
+
+    public void clickBtnSignIn(){
+        Driver.visibilityOf(loginMap.btnSignIn);
+        loginMap.btnSignIn.click();
+    }
+
+    public boolean isBtnSignIn(){
+        Driver.visibilityOf(loginMap.btnSignIn);
+        return loginMap.btnSignIn.isEnabled();
+    }
+
+    public void visibilityOfBtnFechar(){
+        Driver.visibilityOf(loginMap.btnFechar);
+    }
+
+    public void invisibilityOfBtnFechar(){
+        Driver.invisibilityOf(loginMap.btnFechar);
+    }
+
+    public void aguardaLoader(){
+        Driver.attributChange(loginMap.divLoader, "display", "none");
+    }
+
+    public String getUsuarioLogado(){
+        Driver.visibilityOf(loginMap.textLogado);
+        return loginMap.textLogado.getText();
+    }
+
+    public String getErroLogin(){
+        Driver.visibilityOf(loginMap.textErroLogin);
+        return loginMap.textErroLogin.getText();
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,62 @@
+#language:pt
+@login
+Funcionalidade: Login
+
+  Contexto:
+    Dado que a modal esteja sendo exibida
+
+  @fecharModal
+  Cenario: Fechar a modal ao clicar fora da mesma
+    Quando for realizado um clique fora da modal
+    Entao a janela modal deve ser fechada
+
+  @fecharModalIcone
+  Cenario: Fechar a modal ao clicar no ícone fechar
+    Quando for realizado um clique no icone de fechar modal
+    Entao a janela modal deve ser fechada
+
+  @CreateNewAccount
+  Cenario: Link Create New Account
+    Quando for realizado um clique no link Create New Account
+    Entao a pagina Create New Account deve ser exibida
+
+  @loginComSucesso
+  Esquema do Cenario: Realizar login com <identificacao>
+    Quando os campos de login sejam preenchidos da seguinte forma
+      | login    | <login>    |
+      | password | <password> |
+      | remember | <remember> |
+    Quando for realizado o clique no botao sign in
+    Entao deve ser possivel logar no sistema
+
+    Exemplos:
+      | identificacao       | login | password | remember |
+      | campos obrigatorios | ASCT4 | Senha123 | false    |
+      | todos os campos     | ASCT4 | Senha123 | true     |
+
+  @loginInvalido
+  Esquema do Cenario: Realizar login com <identificacao>
+    Quando os campos de login sejam preenchidos da seguinte forma
+      | login    | <login>    |
+      | password | <password> |
+      | remember | <remember> |
+    Quando for realizado o clique no botao sign in
+    Entao o sistema devera exibir uma mensagem de erro
+
+    Exemplos:
+      | identificacao    | login    | password | remember |
+      | usuário invalido | invalido | Senha123 | false    |
+      | senha invalida   | ASCT4    | invalida | false    |
+
+  @dadosEmBranco
+  Esquema do Cenario: Realizar login com <identificacao>
+    Quando os campos de login sejam preenchidos da seguinte forma
+      | login    | <login>    |
+      | password | <password> |
+      | remember | <remember> |
+    Entao o botao sign in deve permanecer desabilitado
+
+    Exemplos:
+      | identificacao     | login | password | remember |
+      | usuario em branco |       | Senha123 | false    |
+      | senha em branco   | ASCT4 |          | false    |
